f7cff2213bd61d58e53b6842fb78c7bf5ac127d1
nealterrell <neal.terrell@csulb.edu>
2016-04-25 16:50:14 -0700

    Renamed functions.

--- Erlang/Fibonacci/src/fibonacci.erl
+++ Erlang/Fibonacci/src/fibonacci.erl
@@ -1,5 +1,5 @@
 -module(fibonacci).
--export([fib/1, fib3serial/1, fib3parallel/1, fibThread/2]).
+-export([fib/1, fibSequential/2, fibParallel/2, fibThread/2]).
 
 fib(0) -> 1;
 fib(1) -> 1;
@@ -7,7 +7,8 @@ fib(N) when N>1 -> fib(N-1) + fib(N-2);
 fib(_) -> undefined.
 
 %% Three fib numbers in serial, as three values in a list.
-fib3serial(N) -> [fib(N), fib(N), fib(N)].
+fibSequential(NumCalcs, FibNum) ->
+  [fib(FibNum) || _ <- lists:seq(1, NumCalcs)].
 
 
 
@@ -21,19 +22,17 @@ fibThread(SendTo, N) ->
 %% A process to calculate 3 fib numbers by spawning 3 threads, sending
 %% each a message, then handling their responses to build a list of
 %% 3 values.
-fib3parallel(N) ->
-  P1 = spawn(?MODULE, fibThread, [self(), N]),
-  P2 = spawn(?MODULE, fibThread, [self(), N]),
-  P3 = spawn(?MODULE, fibThread, [self(), N]),
+fibParallel(NumThreads, FibNum) ->
+  _ = [spawn(?MODULE, fibThread, [self(), FibNum]) || _ <- lists:seq(1, NumThreads)],
   %% Await the replies; 3 expected. Return them.
-  awaitfibs(3, []).
+  awaitfibs(NumThreads, []).
 
 %% A loop to receive a certain number of messages contining fib
-%% results, then returna list containing all the results.
+%% results, then return a list containing all the results.
 awaitfibs(ThreadsLeft, Results) ->
   receive
-    {From, FibN} when ThreadsLeft =:= 1 ->
+    {_, FibN} when ThreadsLeft =:= 1 ->
       [FibN|Results];
-    {From, FibN} ->
+    {_, FibN} ->
       awaitfibs(ThreadsLeft - 1, [FibN|Results])
   end.
